dda4dc29f94815113330e11c524957346e8567ef
Facundo <santillanfacundo43@gmail.com>
2024-08-06 09:18:57 -0300

    Dashboard con router doom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/navbar/Navbar"
 
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Router>
+        <Navbar />
+          <Routes>
+            {/* <Route path="/venta" element={<Venta />} />
+            <Route path="/credito" element={<Credito />} />
+            <Route path="/clientes" element={<Clientes />} />
+            <Route path="/articulo" element={<Articulo />} />
+            <Route path="/cotizar" element={<Cotizar />} /> */}
+          </Routes>
+    </Router>
   )
 }
 
